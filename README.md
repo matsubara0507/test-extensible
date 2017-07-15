a7a597e9594fa2787e53790c2cd80d7a4a70afb7
松原信忠 <t12307043@gunma-u.ac.jp>
2017-07-16 01:13:30 +0900

    Added sample

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Haskell の extensible パッケージのテストリポジトリ
 - [extensible: Extensible, efficient, optics-friendly data types and effects | Hackage](https://hackage.haskell.org/package/extensible)
 - [割とすぐに始められるextensibleチュートリアル(レコード編) - モナドとわたしとコモナド](http://fumieval.hatenablog.com/entry/2016/10/10/000011)
 - [波打たせるものの正体(エクステンシブル・タングル) - モナドとわたしとコモナド](http://fumieval.hatenablog.com/entry/2016/12/18/181540)
+
+## memo
+
+* `(>:)` で何故か怒られる
+    * LTS-8.22 にある extensible のバージョンには `(>:)` は無いため
+    * Nightry-2017.7.15 は最新(extensible-0.4.2)なので変更したらいけた
+    * ちなみに `(>:)` は `(:>)` の型エイリアス(上記のリンク先では `(:>)` を使ってる)
+    * lens の `(:>)` と被るから作ったのかな？？
+* `#name` のように自動生成されるレコード関数のようなのを使うには `OverloadedLabels` が必要
+    * [これ](http://d.hatena.ne.jp/kazu-yamamoto/20160114/1452735514)を自動でやってくれるのね
+    * lens で `hoge ^. #name` や `view #name hoge`
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,36 @@
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TypeFamilies     #-}
+{-# LANGUAGE TypeOperators    #-}
+
 module Main where
 
+import           Control.Lens
+import           Data.Extensible
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello world"
+
+type Book = Record '[ "name" >: String
+                    , "author" >: [String]
+                    , "date" >: String
+                    , "isbm" >: String
+                    , "price" >: Float
+                    ]
+
+book1 :: Book
+book1 = #name @= "Type and Programming Language"
+     <: #author @= ["Benjamin C. Pierce"]
+     <: #date @= "January 2002"
+     <: #isbm @= "9780262162098"
+     <: #price @= 95.00
+     <: emptyRecord
+
+book2 :: Book
+book2 = #name @= "Structure and Interpretation of Computer Programs"
+     <: #author @= ["Harold Abelson", "Gerald Jay Sussman", "Julie Sussman"]
+     <: #date @= "July 1996"
+     <: #isbm @= "9780262510875"
+     <: #price @= 55.00
+     <: emptyRecord
